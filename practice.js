512e47630b75e04bb9b10b37da02938ea27619fa
Nick Adams <nick.pappas.adams@gmail.com>
2022-01-22 10:10:30 -0600

    added fizz buzz

--- practice.js
+++ practice.js
@@ -27,9 +27,21 @@ for (let i = 0; i < 10; i++) {
 
 // is palindrome
 const reverseString = (string) => {
-    const reverseString = string.split("").reverse().join("")
-    return string === reverseString
+    const reverseString = string.split("").reverse().join("");
+    return string === reverseString;
 }
 
+console.log(reverseString(string));
 
-console.log(reverseString("amanaplanacanalpanama"));
+
+// fizz buzz
+const fizzBuzz = (n) => {
+    for (let i = 0; i <= n; i++) {
+        if (i % 15 === 0) console.log("FizzBuzz");
+        else if (i % 3 === 0) console.log("Fizz");
+        else if (i % 5 === 0) console.log("Buzz");
+        else console.log(i);
+    }
+}
+
+fizzBuzz(50);
