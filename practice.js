f8df59c12b50e16f07d0289db5f19045fff89d03
Nick Adams <nick.pappas.adams@gmail.com>
2022-01-22 10:00:35 -0600

    added fibonachi

--- practice.js
+++ practice.js
@@ -6,3 +6,21 @@ console.log(string);
 
 const newString = string.replace(/[aeiou]/ig, "");
 console.log(newString);
+
+
+// fibonachi
+const fib = (n) => {
+    let a = 0;
+    let b = 1;
+    for(let i = 0; i < n; i++) {
+        let x = a;
+        a = b;
+        b = b + x;
+
+    }
+    return a;
+}
+
+for(let i = 0; i < 10; i++) {
+    console.log(fib(i));
+}
