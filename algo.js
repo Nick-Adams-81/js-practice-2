89262867b5c445bcfd97af29bff8d4cd0fb17c40
Nick Adams <nick.pappas.adams@gmail.com>
2022-04-13 13:42:27 -0500

    added boolean filter function

--- algo.js
+++ algo.js
@@ -30,3 +30,7 @@ const frankenSplice = (arr1, arr2, n) => {
 }
 
 console.log(frankenSplice([1,2,3], [4,5],1));
+
+// remove false values from an array
+const bouncer = arr => arr.filter(Boolean);
+console.log(bouncer(["hello", 4, false, "hi"]));
