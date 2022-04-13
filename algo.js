a6a85f9d209aa52e19588285b838c0c0ad8db0f5
Nick Adams <nick.pappas.adams@gmail.com>
2022-04-12 20:17:00 -0500

    added find el function

--- algo.js
+++ algo.js
@@ -1,7 +1,17 @@
 "use strict";
 
+// truncate a string
 const truncateString = (str, num) => {
     return str.length > num ? str.slice(0, num) + "..." : str;
 }
 
 console.log(truncateString("hello World", 8));
+
+// finders keepers
+const findEl = (arr, func) => {
+    return arr.find(func);
+}
+
+const modulus = num =>  num % 2 === 0;
+
+console.log(findEl([1, 3, 5, 9, 10], modulus))
