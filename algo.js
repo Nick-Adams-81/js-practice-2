dda541fd2dbea292a0441bdc33f3f313cb01598e
Nick Adams <nick.pappas.adams@gmail.com>
2022-04-12 20:19:33 -0500

    added boolean method

--- algo.js
+++ algo.js
@@ -4,14 +4,16 @@
 const truncateString = (str, num) => {
     return str.length > num ? str.slice(0, num) + "..." : str;
 }
-
 console.log(truncateString("hello World", 8));
 
 // finders keepers
 const findEl = (arr, func) => {
     return arr.find(func);
 }
-
+// modulus operator function to plug into find el function
 const modulus = num =>  num % 2 === 0;
+console.log(findEl([1, 3, 5, 9, 10], modulus));
 
-console.log(findEl([1, 3, 5, 9, 10], modulus))
+// check if typeof input is a boolean
+const booWho = bool => typeof bool === "boolean"
+console.log(booWho(false));
