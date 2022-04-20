22f365b1044e1d57e59c86b5c421bdb2d61866f4
Nick Adams <nick.pappas.adams@gmail.com>
2022-04-20 17:39:18 -0500

    added async await using fetch

--- domStuff.js
+++ domStuff.js
@@ -1,12 +1,32 @@
 "use strict";
+
 let newBody = document.getElementById("test-body");
 // create new element and appending it to the html
 let newEl = document.createElement("input");
 document.body.appendChild(newEl);
 
+// creating a button element
 let btn = document.createElement("button");
 btn.innerText = "click me";
 btn.addEventListener("click", () => {
     alert("you clicked me!!!");
-})
+});
 newBody.appendChild(btn);
+
+// fetch api with async await
+const loadUsers = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users")
+    const users = await res.json();
+    return users;
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+    let users = []
+    try {
+        users = await loadUsers();
+    } catch (err) {
+        console.log({ message: err.message })
+    }
+    console.log(users);
+})
+
