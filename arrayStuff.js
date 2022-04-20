6af1fd3d7987d18df21048d10ee1bd4df646a2a6
Nick Adams <nick.pappas.adams@gmail.com>
2022-04-20 13:29:35 -0500

    refactored arrsyStuff file

--- arrayStuff.js
+++ arrayStuff.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// Map //
+
 
 // array for testing 
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -12,18 +12,19 @@ const salesPeeps = [
     {name: "davey pageturns", sales: 450},
     {name: "Ben Sisko", sales: 225}
 ];
+// ---Map--- //
 
 // using map to transform array[arr]
 let increment = arr.map(n => n + 1);
 console.log(increment);
 
-// filter //
+// ---Filter--- //
 
 // using filter to sort even numbers from array[arr]
 let evens = arr.filter(n => n % 2 === 0);
 console.log(evens);
 
-// reduce //
+// ---Reduce--- //
 
 // reducing the array[arr] to single value
 let sum = arr.reduce((accumulation, currentNums) => {return accumulation + currentNums}, 0);
