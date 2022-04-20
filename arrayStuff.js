ba321e078a2aa8ef7de6444289cd0a2741326088
Nick Adams <nick.pappas.adams@gmail.com>
2022-04-20 13:08:14 -0500

    added map and filter

---
+++ arrayStuff.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// Map //
+
+// array for testing 
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// using map to transform array[arr]
+let increment = arr.map(n => n + 1);
+console.log(increment);
+
+// filter //
+let evens = arr.filter(n => n % 2 === 0)
+console.log(evens)
+
+
