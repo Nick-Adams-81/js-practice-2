5e476f4818689853363c7c9c24d5b491ca68427b
Nick Adams <nick.pappas.adams@gmail.com>
2022-04-24 12:58:48 -0500

    added reverse string

--- js-test.js
+++ js-test.js
@@ -32,4 +32,7 @@ const fizzBuzz = n => {
 const stripString = str => str.replace(/[aeiou]/ig, "");
 console.log(stripString("hellO crUel world"));
 
+// reverse a string
+const reverseString = str => str.split("").reverse().join("");
+console.log(reverseString("hello"));
 
