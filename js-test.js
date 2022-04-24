ffda37561a1bf7e2aa621f0c9b0ad25bdf7c61ec
Nick Adams <nick.pappas.adams@gmail.com>
2022-04-24 12:54:32 -0500

    added fizzbuzz test

--- js-test.js
+++ js-test.js
@@ -2,9 +2,9 @@
 
 // fibonachi
 const fib = n => {
-    let a = 0; 
+    let a = 0;
     let b = 1;
-    for(let i = 0; i < n; i++) {
+    for (let i = 0; i < n; i++) {
         let x = a;
         a = b;
         b = b + x;
@@ -12,6 +12,20 @@ const fib = n => {
     return a;
 }
 
-for(let i = 0; i < 10; i++) {
+for (let i = 0; i < 10; i++) {
     console.log(fib(i));
 }
+
+// fizz buzz
+const fizzBuzz = n => {
+    for (let i = 0; i <= n; i++) {
+        if (i % 15 === 0) console.log("FizzBuzz");
+        else if (i % 3 === 0) console.log("Fizz");
+        else if (i % 5 === 0) console.log("Buzz");
+        else console.log(i);
+    }
+}
+
+fizzBuzz(50);
+
+
