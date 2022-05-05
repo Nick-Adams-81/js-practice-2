9f9b40f65d93f4be021ed4c5c8c3dfbf99831534
Nick Adams <nick.pappas.adams@gmail.com>
2022-05-05 14:28:31 -0500

    added ajax call to json placeholder

--- jquery-practice.js
+++ jquery-practice.js
@@ -1,9 +1,28 @@
 $(document).ready(function() {
+
+    // basic button and making background color in jquery
     var button = document.createElement("button")
     button.innerText = "click me"
     document.body.append(button)
     $("body").css("background-color", "green")
     $(button).click(function() {
         alert("ive been clicked")
+    });
+
+    // ajax call to json placeholder
+    $.ajax("https://jsonplaceholder.typicode.com/users")
+    .done(function(data) {
+        console.log("much success", data)
     })
+    .fail(function(err) {
+        console.log("something broke!", err)
+    })
+
+
+
+
+
+
+
+
 })
