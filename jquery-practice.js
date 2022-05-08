4fbc428d97266c7d246f274b9519bf2248405c95
Nick Adams <nick.pappas.adams@gmail.com>
2022-05-08 14:53:28 -0500

    added appended divs to dom

--- jquery-practice.js
+++ jquery-practice.js
@@ -1,35 +1,53 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     // basic button and making background color in jquery
     var button = document.createElement("button")
+    let nameInput = document.createElement("input")
+    let cityInput = document.createElement("input")
+    let form = document.createElement("form")
+    let name = document.createElement("h2")
+    let email = document.createElement("p")
     button.innerText = "click me"
-    document.body.append(button)
+    form.append(button)
+    form.append(nameInput)
+    form.append(cityInput)
+    document.body.append(form)
+
     $("body").css("background-color", "green")
-    $(button).click(function() {
+    $(button).click(function () {
         alert("ive been clicked")
+
     });
+
 
     // ajax call to json placeholder
     $.ajax("https://jsonplaceholder.typicode.com/users")
-    .done(function(data) {
-        console.log("much success", data)
-    })
-    .fail(function(err) {
-        console.log("something broke!", err)
-    })
+        .done(function (data) {
+            console.log("much success", data)
+            data.map(function(data) {
+                name.innerText = data.name
+                email.innerText = data.email
+                document.body.append(name, email)
+            })
+         
+        })
+        .fail(function (err) {
+            console.log("something broke!", err)
+        })
 
-    $.post("https://jsonplaceholder.typicode.com/posts", {
+
+    jQuery.post("https://jsonplaceholder.typicode.com/posts", {
         name: "Nick",
         city: "Dallas",
         state: "Texas",
         street: "3403 Mcneil Street"
     })
-    .done(function(data) {
-        console.log(data)
-    })
-    .fail(function(err) {
-        console.log(err)
-    })
+        .done(function (data) {
+            console.log(data)
+        })
+        .fail(function (err) {
+            console.log(err)
+        })
 
 
 
